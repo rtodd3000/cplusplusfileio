fb5d07ca86708c858fb4e4431efc08ae0da51ae0
Ryan Todd <rtodd@chapman.edu>
2023-10-04 19:03:10 -0700

    Adding all components of filecap assignment

--- filecap.cpp
+++ filecap.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <stdio.h>
-#include <ctype.h>
 using namespace std;
-int main(){
+int main(int argc, char *argv[]){
     ifstream og_file;
+    ofstream out_file;
     string line;
+    fstream fin;
+
+    
     og_file.open("test.txt");
-    while(getline(og_file,line)){
-        for (char curChar : line){
-            if (isupper(curChar)){
-                putchar (tolower(curChar));
-            }
-            else if (islower(curChar)){
-                putchar (toupper(curChar));
-            }
-            // else{
-            //     cout << curChar << end;
-            // }
+    // creates new file under command line name given by user
+    out_file.open(argv[1]);
+
+    // error handling to make sure our original test.txt file exists
+    try{
+        fin.open("test.txt", ios::in);
+        if(!fin){
+            throw string("file does not exist 1\n");
         }
     }
-    cout << line << endl;
-    og_file.close();
+    catch(string err){
+        cout << err;
+    }
+    // While loop that checks for case sensitivity
+    while(getline(og_file,line)){
+        char changedChar; // char variable that will output to new file
+        for (char curChar : line){ // for every character on the line in the test.txt
+            if (isupper(curChar)){ // checks if character is uppercase
+                changedChar = tolower(curChar);
+                out_file << changedChar; //outputs opposite case
+                
+            }
+            else if (islower(curChar)){
+                changedChar = toupper(curChar); // checks if character is lowercase
+                out_file << changedChar; // outputs opposite case
+            }
+            else{
+                out_file << curChar; //if not a letter, outputs char as is
+            }
+        }
+    }
+    out_file.close(); 
     return 0;
 }
